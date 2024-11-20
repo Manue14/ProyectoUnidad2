2dc208cb1cf48c1a2b1da463ebba0c561c9b537b
Samugcor <samueligcorp@gmail.com>
2024-11-20 18:47:11 +0100

    Controllers arreglados???

---
+++ src/main/java/org/example/proyectounidad2/Controller/AdminController.java
@@ -0,0 +1,52 @@
+package org.example.proyectounidad2.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class AdminController {
+
+    @FXML
+    private Button btn_addObras;
+
+    @FXML
+    private Button btn_addObras1;
+
+    @FXML
+    private ComboBox<?> cmb_categoria;
+
+    @FXML
+    private ComboBox<?> cmb_categoria1;
+
+    @FXML
+    private ComboBox<?> cmb_departamento;
+
+    @FXML
+    private ComboBox<?> cmb_departamento1;
+
+    @FXML
+    private ComboBox<?> cmb_movimiento;
+
+    @FXML
+    private ComboBox<?> cmb_movimiento1;
+
+    @FXML
+    private TextField tf_autor;
+
+    @FXML
+    private TextField tf_autor1;
+
+    @FXML
+    private TextField tf_titulo;
+
+    @FXML
+    private TextField tf_titulo1;
+
+    @FXML
+    void mostrarAddObra(KeyEvent event) {
+
+    }
+
+}
