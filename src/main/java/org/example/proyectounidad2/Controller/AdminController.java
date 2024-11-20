c188988bdfb9a4668a3dcd95daed0aa1fa537659
Samugcor <samueligcorp@gmail.com>
2024-11-20 19:14:04 +0100

    Controllers arreglado

--- src/main/java/org/example/proyectounidad2/Controller/AdminController.java
+++ src/main/java/org/example/proyectounidad2/Controller/AdminController.java
@@ -5,44 +5,58 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class AdminController {
+
+    @FXML
+    private Button btn_addAutores;
 
     @FXML
     private Button btn_addObras;
 
     @FXML
-    private Button btn_addObras1;
+    private Button btn_buscarAutor;
+
+    @FXML
+    private Button btn_buscarObra;
 
     @FXML
     private ComboBox<?> cmb_categoria;
 
     @FXML
-    private ComboBox<?> cmb_categoria1;
-
-    @FXML
     private ComboBox<?> cmb_departamento;
-
-    @FXML
-    private ComboBox<?> cmb_departamento1;
 
     @FXML
     private ComboBox<?> cmb_movimiento;
 
     @FXML
-    private ComboBox<?> cmb_movimiento1;
+    private ComboBox<?> cmb_nacionalidad;
+
+    @FXML
+    private TextField tf_apellido1;
+
+    @FXML
+    private TextField tf_apellido2;
 
     @FXML
     private TextField tf_autor;
 
     @FXML
-    private TextField tf_autor1;
+    private TextField tf_nombre;
 
     @FXML
     private TextField tf_titulo;
 
     @FXML
-    private TextField tf_titulo1;
+    void buscarAutor(MouseEvent event) {
+
+    }
+
+    @FXML
+    void buscarObra(MouseEvent event) {
+
+    }
 
     @FXML
     void mostrarAddObra(KeyEvent event) {
